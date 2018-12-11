4cf79d67dd623e98881e030c4399b9aad1f8250a
Tomasz Rycerz <tomryc@gmail.com>
2018-12-11 16:34:51 +0000

    adding Add Book buuton and panes for all buttons. Closes #4

--- src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/MainController.java
+++ src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/MainController.java
@@ -1,7 +1,12 @@
 package com.tomryc.projects.javafx.myownlibrary.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
  * Created by Tomasz Rycerz on 11/12/2018
@@ -16,8 +21,21 @@ public class MainController {
 
     @FXML
     private void initialize(){
-
         topMenuButtonsController.setMainController(this);
+    }
+
+    public void setCenter(String fxmlPath){
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(parent);
 
     }
 }
--- src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/TopMenuButtonsController.java
+++ src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/TopMenuButtonsController.java
@@ -1,28 +1,48 @@
 package com.tomryc.projects.javafx.myownlibrary.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * Created by Tomasz Rycerz on 11/12/2018
  */
 public class TopMenuButtonsController {
 
+    public static final String LIBRARY_FXML = "/fxml/Library.fxml";
+    public static final String LIST_BOOKS_FXML = "/fxml/ListBooks.fxml";
+    public static final String STATISTICS_FXML = "/fxml/Statistics.fxml";
+    public static final String ADD_BOOK_FXML = "/fxml/AddBook.fxml";
     private MainController mainController;
 
     @FXML
-    public void openLibrary() {
+    private ToggleGroup toggleButtons;
 
+    @FXML
+    public void openLibrary() {
+        mainController.setCenter(LIBRARY_FXML);
     }
+
     @FXML
     public void openListBooks() {
-
+        mainController.setCenter(LIST_BOOKS_FXML);
     }
+
     @FXML
     public void openStatistics() {
+        mainController.setCenter(STATISTICS_FXML);
+    }
 
+    @FXML
+    public void addBook() {
+        if(toggleButtons.getSelectedToggle()!=null){
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
+        mainController.setCenter(ADD_BOOK_FXML);
     }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
+
+
 }
