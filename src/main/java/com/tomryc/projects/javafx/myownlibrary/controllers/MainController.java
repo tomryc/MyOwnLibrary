c011aed670a8a038cd354d1c8678c03a780f8a42
Tomasz Rycerz <tomryc@gmail.com>
2018-12-11 12:43:19 +0000

    create buttons top menu

--- src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/MainController.java
+++ src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/MainController.java
@@ -1,7 +1,23 @@
 package com.tomryc.projects.javafx.myownlibrary.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 /**
  * Created by Tomasz Rycerz on 11/12/2018
  */
 public class MainController {
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+
+    @FXML
+    private void initialize(){
+
+        topMenuButtonsController.setMainController(this);
+
+    }
 }
---
+++ src/main/java/com/tomryc/projects/javafx/myownlibrary/controllers/TopMenuButtonsController.java
@@ -0,0 +1,28 @@
+package com.tomryc.projects.javafx.myownlibrary.controllers;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Tomasz Rycerz on 11/12/2018
+ */
+public class TopMenuButtonsController {
+
+    private MainController mainController;
+
+    @FXML
+    public void openLibrary() {
+
+    }
+    @FXML
+    public void openListBooks() {
+
+    }
+    @FXML
+    public void openStatistics() {
+
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+}
